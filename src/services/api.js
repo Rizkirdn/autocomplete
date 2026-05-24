5a1a406e0c1fd36bac4627af4a18af078341268e
rizki <rizkiramdaninugraha@gmail.com>
2026-05-24 09:30:45 +0700

    api resolve

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:5000/api'
+  : `${window.location.origin}/api`;
 
 async function fetchAPI(endpoint, options = {}) {
   const res = await fetch(`${API_URL}${endpoint}`, {
